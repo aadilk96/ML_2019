409a035cb43cd7da6fd422cfdf3a007892fd0f3b
aadilk96 <aadilanilkumar@gmail.com>
2019-03-13 11:22:30 +0100

    ++python file

--- Project 1/notes.py
+++ Project 1/notes.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+
 # coding: utf-8
 
 # # Machine Learning
@@ -14,7 +14,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-# # Loading data
+# ## Loading data
 
 # In[2]:
 
@@ -32,64 +32,72 @@ def showImage(vectors):
     imageMatrix = vectors.reshape(16, 15)
     plt.imshow(imageMatrix, cmap = 'gray')
     plt.show()
-def imageCat(vectors): 
+    
+def multImage(vectors): 
     for rows in vectors: 
         showImage(rows)
 
 
-# ## Implementation of k-means algorithm
+# ## Implementation of K-means algorithm
 
 # In[4]:
 
 
-def distance(p1, p2):
+def euclideanDistance(p1, p2):
     diff = p1 - p2
     s = np.sum(np.power(diff, 2))
     return np.sqrt(s)
 
 
+# #### Returns the index of the points with the lowest distance to the centroids
+
 # In[5]:
 
 
-def classify(pixels, centers): 
-    num_centers, _ = centers.shape
-    num_pixels, _ = pixels.shape
-    distances = np.zeros((num_centers, num_pixels))
+def classify(pixels, centroids): 
+    numcentroids, _ = centroids.shape
+    numPixels, _ = pixels.shape
+    distances = np.zeros((numcentroids, numPixels))
     
-    for i in range(num_centers): 
-        for j in range(num_pixels): 
-            distances[i][j] = distance(centers[i], pixels[j])
+    for i in range(numcentroids): 
+        for j in range(numPixels): 
+            distances[i][j] = euclideanDistance(centroids[i], pixels[j])
+            
     return np.argmin(distances, axis = 0)
 
+
+# #### Returns the center points of the clusters
 
 # In[6]:
 
 
-def find_cluster(pixels, centers): 
-    num_centers, _ = centers.shape
-    num_pixels, dim = pixels.shape
+def findCluster(pixels, centroids): 
+    numcentroids, _ = centroids.shape
+    numPixels, dim = pixels.shape
+    classified = classify(pixels, centroids)
     
-    classified = classify(pixels, centers)
-    for c in range(num_centers): 
+    for c in range(numcentroids): 
         sum = np.zeros(dim)
         count = 0 
-        for i in range(num_pixels): 
+        for i in range(numPixels): 
             if(classified[i] == c): 
                 sum += pixels[i]
                 count += 1
+                
         mean = sum / count 
-        centers[c] = mean
-    return centers 
+        centroids[c] = mean
+        
+    return centroids 
 
 
 # In[7]:
 
 
-def k_means(pixels, k, iterations): 
-    centers = pixels[0:k]
+def kMeans(pixels, k, iterations): 
+    centroids = pixels[0:k]
     for i in range(iterations): 
-        centers = find_cluster(pixels, centers)
-    return centers
+        centroids = findCluster(pixels, centroids)
+    return centroids
 
 
 # ## k = 1
@@ -97,17 +105,36 @@ def k_means(pixels, k, iterations):
 # In[8]:
 
 
-centers = k_means(zeros, 1, 10)
-imageCat(centers)
+centroids = kMeans(zeros, 1, 10)
+multImage(centroids)
 
+mean = np.mean(zeros, axis = 0)
+for i in range(10): 
+    centroid = kMeans(zeros, 1, i)
+    print("Euclidean Distance:", euclideanDistance(mean, centroid))
+    plt.plot(i, euclideanDistance(mean, centroid), 'rx')
+    plt.ylabel('Euclidean Distance')
+    plt.xlabel('Iterations')
+    plt.show
+
+
+# #### As you can see the distance of the mean to centroid is 0 which shows that for K = 1 the centroid is just the mean, even for multiple iterations.  
 
 # ## k = 2
 
 # In[9]:
 
 
-centers = k_means(zeros, 2, 10)
-imageCat(centers)
+centroids = kMeans(zeros, 2, 10)
+multImage(centroids)
+#mean = np.mean(zeros, axis = 0)
+for i in range(10): 
+    centroid = kMeans(zeros, 2, i)
+    print("Euclidean Distance:", euclideanDistance(mean, centroid))
+    plt.plot(i, euclideanDistance(mean, centroid), 'rx')
+    plt.ylabel('Euclidean Distance')
+    plt.xlabel('Iterations')
+    plt.show
 
 
 # ## k = 3
@@ -115,8 +142,16 @@ imageCat(centers)
 # In[10]:
 
 
-centers = k_means(zeros, 3, 10)
-imageCat(centers)
+centroids = kMeans(zeros, 3, 10)
+multImage(centroids)
+#mean = np.mean(zeros, axis = 0)
+for i in range(10): 
+    centroid = kMeans(zeros, 3, i)
+    print("Euclidean Distance:", euclideanDistance(mean, centroid))
+    plt.plot(i, euclideanDistance(mean, centroid), 'rx')
+    plt.ylabel('Euclidean Distance')
+    plt.xlabel('Iterations')
+    plt.show
 
 
 # ## k = 200
@@ -124,6 +159,14 @@ imageCat(centers)
 # In[11]:
 
 
-centers = k_means(zeros, 200, 10)
-imageCat(centers)
+centroids = kMeans(zeros, 200, 1)
+multImage(centroids)
+#mean = np.mean(zeros, axis = 0)
+for i in range(10): 
+    centroid = kMeans(zeros, 200, i)
+    print("Euclidean Distance:", euclideanDistance(mean, centroid))
+    plt.plot(i, euclideanDistance(mean, centroid), 'rx')
+    plt.ylabel('Euclidean Distance')
+    plt.xlabel('Iterations')
+    plt.show
 
