6fb225c748736e49f955407a04714559b1ad515a
dragikamov <dragikamov@gmail.com>
2019-04-21 17:00:42 +0200

    Changed formulas

--- Project 2/notes.py
+++ Project 2/notes.py
@@ -7,7 +7,7 @@
 
 # ## Used libraries
 
-# In[1]:
+# In[3]:
 
 
 import numpy as np
@@ -18,7 +18,7 @@ from sklearn.decomposition import PCA
 
 # ## Loading data
 
-# In[2]:
+# In[4]:
 
 
 vectors = np.loadtxt('data/mfeat-pix.txt')
@@ -61,9 +61,9 @@ vectors = np.loadtxt('data/mfeat-pix.txt')
 
 # ## Adding bias
 # 
-# We then create a fuction to add the bias term to the features. Linear regression will create a model based on offine function, which contains a bais term. Without it, we can only approximate the data using linear function, and it would lead to a very bad model.
+# We then create a fuction to add the bias term to the features. Linear regression will create a model based on offine function, which contains a bias term. Without it, we can only approximate the data using linear function, and it would lead to a very bad model.
 
-# In[3]:
+# In[5]:
 
 
 def add_bias(X):
@@ -71,6 +71,7 @@ def add_bias(X):
     Y = np.ones((N, D + 1))
     Y[:,:-1] = X
     return Y
+
 def square_norm(x):
     return np.sum(np.power(x, 2))
 
@@ -78,7 +79,7 @@ def square_norm(x):
 # ## Use One-hot encoding to generate class vector
 # Since the dataset doesn't contain any kind of label, we need to generate a class vector for each label $\{0, 1, 2, ... 10\}$ as $v \in \mathbb{R}^{10}$. The following function will execute such a strategy.
 
-# In[4]:
+# In[6]:
 
 
 def onehot_encode(digit):
@@ -95,20 +96,20 @@ def onehot_encode(digit):
 # 3. Associate $X$ and $X_{test}$ with bias term, thus we have $X, X_{test} \in \mathbb{R}^{1000 \times (K + 1)}$ 
 # 4. Build the correct class vector for training set as $Y \in \mathbb{R}^{(k + 1) \times 10}$ and test set as $Y_{test} \in \mathbb{R}^{(k + 1) \times 10}$. After such operation, we obtained the complete training set as $(X, Y)$ and the test set as $(X_{test}, Y_{test})$
 # 5. Using the training set, compute the optimal weight matrix as
-# $$ {W_{opt}}^\top = (\frac{1}{N} \cdot X \cdot X^\top + \alpha^2 \cdot I_{nxn})^{-1} \cdot \frac{1}{N} \cdot X \cdot Y $$
+# $$ {W_{opt}}^\top = (\frac{1}{N} \cdot X^\top \cdot X + \alpha^2 \cdot I_{nxn})^{-1} \cdot \frac{1}{N} \cdot X^\top \cdot Y $$
 # we can rewrite as
-# $$ W_{opt} = ((\frac{1}{N} \cdot X \cdot X^\top + \alpha^2 \cdot I_{nxn})^{-1} \cdot \frac{1}{N} \cdot X \cdot Y)^\top $$
+# $$ W_{opt} = ((\frac{1}{N} \cdot X^\top \cdot X + \alpha^2 \cdot I_{nxn})^{-1} \cdot \frac{1}{N} \cdot X^\top \cdot Y)^\top $$
 # 6. Calculate the error term.
 # First, we make the prediction:
-# $$ Y_{pred} = (W_{opt} \cdot X)^\top $$
-# $$ Y_{test}pred = (W_{opt} \cdot X_{test})^\top $$
+# $$ Y_{pred} = (W_{opt} \cdot X^\top)^\top $$
+# $$ Y_{test}pred = (W_{opt} \cdot {X_{test}}^\top)^\top $$
 # Using the prediction, we calculate the corresponding error
 # $$ MSE_{train} = \frac{\|Y - Ypred\|^2}{1000} $$
 # $$ MSE_{test} = \frac{\|Ytest - Y_{test}pred\|^2}{1000} $$
 # $$ MISS_{train} = \frac{\sum_{i = 1}^{1000} \min(1, \|\arg\max(Y_i) - \arg\max(Ypred_i)\|)}{1000} $$
 # $$ MISS_{test} = \frac{\sum_{i = 1}^{1000} \min(1, \|\arg\max({Y_{test}}_i) - \arg\max(Y_{test}pred_i)\|)}{1000} $$
 
-# In[5]:
+# In[126]:
 
 
 def linear_regression(data, k):
@@ -128,42 +129,47 @@ def linear_regression(data, k):
     Xtest = add_bias(Xtest)
     
     # Building the class
-    Y = np.zeros((k + 1, 10))
-    Ytest = np.zeros((k + 1, 10))
+    Y = np.zeros((1000, 10))
+    Ytest = np.zeros((1000, 10))
     
-    for i in range(k + 1):
+    for i in range(1000):
         digit = i // 100
         Y[i] = onehot_encode(digit).T # Assigning the one-hot encoding
         Ytest[i] = onehot_encode(digit).T # Also here
-        
+
+    left = la.inv((1/1000) * np.matmul(X.T, X) + (0 * np.eye(k+1))) * (1/1000)
+    right = np.matmul(X.T, Y)
+
     # Calculating the optimal weight
-    Wopt = (np.dot(la.inv((1/1000) * np.matmul(X, X.T) + (1000 * np.eye(1000))) * (1/1000), np.matmul(X, Y))).T
+    Wopt = (np.dot(left,right)).T
     
     # Calculating the training error term
     # We first need to make a prediction
-    Ypred = np.matmul(Wopt, X).T
-    Ytestpred = np.matmul(Wopt, Xtest).T
+    Ypred = np.matmul(Wopt, X.T).T
+    Ytestpred = np.matmul(Wopt, Xtest.T).T
     
     # Then we need to calculate the error
     mse_train = square_norm(Ypred - Y) / 1000.0
     num_miss_train = 0
 
-    for i in range(k+1):
+    for i in range(1000):
         if np.argmax(Ypred[i]) != np.argmax(Y[i]):
             num_miss_train = num_miss_train + 1
     miss_train = num_miss_train / 1000.0
     
     mse_test = square_norm(Ytestpred - Ytest) / 1000.0
     num_miss_test = 0
-    for i in range(k+1):
+    
+    for i in range(1000):
         if np.argmax(Ytestpred[i]) != np.argmax(Ytest[i]):
             num_miss_test = num_miss_test + 1
+            
     miss_test = num_miss_test / 1000.0
     
     return Wopt, mse_train, miss_train, mse_test, miss_test
 
 
-# In[6]:
+# In[127]:
 
 
 _, mse_train, miss_train, mse_test, miss_test = linear_regression(vectors, 40)
@@ -174,7 +180,7 @@ print('mse_train = {}, miss_train = {}, mse_test = {}, miss_test = {}'.format(ms
 # We variate k and check to see how the error changes
 # 1. First, plot the data using linear scaling
 
-# In[7]:
+# In[128]:
 
 
 # Plotting the k and the errors
@@ -190,7 +196,7 @@ for k in range(1, 241):
     miss_trains.append(miss_train)
     mse_tests.append(mse_test)
     miss_tests.append(miss_test)
-    
+
 plt.xlabel('k')
 plt.ylabel('error')
 plt.title('Bias-Variance, Linear Scaling')
@@ -205,7 +211,7 @@ plt.show()
 
 # 2. Then, use log-log scaling of the data
 
-# In[8]:
+# In[129]:
 
 
 # Log plot
@@ -221,8 +227,16 @@ plt.legend(handles=[p1, p2, p3, p4])
 plt.show()
 
 
-# In[ ]:
+# In[32]:
 
 
+_, mse_train, miss_train, mse_test, miss_test = linear_regression(vectors, 1)
+print("mse_train =", mse_train, ", miss_train =", miss_train, ", mse_test =", mse_test, ", miss_test =",miss_test)
 
+
+# In[33]:
+
+
+_, mse_train, miss_train, mse_test, miss_test = linear_regression(vectors, 240)
+print("mse_train =", mse_train, ", miss_train =", miss_train, ", mse_test =", mse_test, ", miss_test =",miss_test)
 
